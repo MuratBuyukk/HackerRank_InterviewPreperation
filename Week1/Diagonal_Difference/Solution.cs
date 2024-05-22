9f32828695ab8fb211f8968b0b1aefd770087154
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-05-22 19:48:19 +0300

    HackerRank Problem Solving..!

---
+++ Week1/Diagonal_Difference/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_Difference
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int leftToRight = 0;
+            int rightToLeft = 0;
+            int index = 0;
+            while (index < arr.Count)
+            {
+                leftToRight += arr[index][index];
+                rightToLeft += arr[index][arr.Count-1-index];
+                index++;
+            }
+            return Math.Abs(leftToRight-rightToLeft);
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.diagonalDifference(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ Week1/Pangrams/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangrams
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'pangrams' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string pangrams(string s)
+        {
+            HashSet<char> result = new HashSet<char>();
+            
+            foreach (char c in s.ToLower())
+            {
+                result.Add(c);
+            }
+            
+            return result.Count == 27 ? "pangram" : "not pangram";
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.pangrams(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
